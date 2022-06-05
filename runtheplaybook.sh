afc8c32feba46bc6a716045ba0ef9206ceeff8a8
Avi Berger <entebox@gmail.com>
2022-06-05 16:55:46 +0000

    added gitignore and created vars files

--- runtheplaybook.sh
+++ runtheplaybook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-start_script=$(date +"%s")
-ansible-playbook ./playbooks/provision.yml --ask-vault-pass -vvv
-end_script=$(date +"%s")
-runtime=$(($end_script-$start_script))
-echo "$(($runtime / 60)) minutes and $(($runtime % 60)) seconds elapsed for Script Execution."
+start_script=$(date +"%s") # get the start time and date
+ansible-playbook ./playbooks/provision.yml --vault-password-file vaultpassfile -vvv # run the playbook with debug option (-vvv)
+end_script=$(date +"%s") # get the end time and date
+runtime=$(($end_script-$start_script)) # get the delta between both of the times taken
+echo -e "\033[1;34m(($runtime / 60)) minutes and $(($runtime % 60)) seconds elapsed for Script Execution." # prints the minutes and seconds of the result
