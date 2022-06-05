83b732021d8cde46eedca6127c3b42c1f4689dcc
Avi Berger <entebox@gmail.com>
2022-06-05 19:09:07 +0000

    small finishes and better time measure script

--- runtheplaybook.sh
+++ runtheplaybook.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-start_script=$(date +"%s") # get the start time and date
-ansible-playbook ./playbooks/provision.yml --vault-password-file vaultpassfile -vvv # run the playbook with debug option (-vvv)
+start_script=$(date +%s.%N) # get the start time and date
+ansible-playbook ./playbooks/provision.yml -i inventory.yaml --vault-password-file vaultpassfile # run the playbook with debug option (-vvv)
 end_script=$(date +"%s") # get the end time and date
-runtime=$(($end_script-$start_script)) # get the delta between both of the times taken
-echo -e "\033[1;34m$(($runtime / 60)) minutes and $(($runtime % 60)) seconds elapsed for Script Execution." # prints the minutes and seconds of the result
+runtime=$(echo "$(date +%s.%N) - $start_script" | bc) # get the delta between both of the times taken
+running_time=`printf "%.2f seconds" $runtime`
+echo -e "\033[1;34mRunning Time: $running_time" # prints the minutes and seconds of the result
